4fbf7644bc4f2223199cbb74ca2284085ee61dfe
DiegoAMA <67812055+DiegoAMA@users.noreply.github.com>
2021-07-16 17:39:12 -0500

    all_ggplot

--- Análisis Cinemático/PROGRAMA TESIS EN R %Ciclo.R
+++ Análisis Cinemático/PROGRAMA TESIS EN R %Ciclo.R
@@ -23,7 +23,6 @@ cycle_process<-function(dataframe){
   ciclos
 } #Extracción y procesamiento de ciclos
 cycles_plot<-function(ciclos_procesados){
-  
   num_ci<-length(ciclos_procesados)
   color<-randomColor(num_ci)
   plot(ciclos_procesados[[1]],type="l",
@@ -34,7 +33,7 @@ cycles_plot<-function(ciclos_procesados){
   for (i in seq_along(1:(num_ci-1))) {
     lines(ciclos[[i+1]],col=color[i+1])  
   }
-  
+
   
 } #Ploteo de todos los ciclos
 erase_cycles<-function(cycles,...){
@@ -42,6 +41,22 @@ erase_cycles<-function(cycles,...){
   cycles[c(v)] <- NULL  
   cycles
 }#Eliminación de ciclos
+all_ggplot<-function(df){
+  num_ci<-length(df)-1
+  color<-randomColor(num_ci)
+  tema<-theme(axis.text=element_text(size=20),
+              axis.title=element_text(size=22,face="bold"),
+              plot.title = element_text(size = 30, face = "bold"))
+  plot<-ggplot(data=C,aes(x=C[,1],y=C[,2],col=color[1]))+
+    geom_line(show.legend = FALSE)+
+    xlab("Ciclo [%]")+
+    ylab("Aceleración [g]")+
+    ggtitle("Ciclos Obtenidos")+tema
+  for (i in seq_along(1:(num_ci-1))) {
+    plot <- plot + geom_line(aes_string(y = names(df)[i+1]),col=color[i+1])
+  }
+  plot
+}
 
 #Importación datos
 setwd("C:/Users/wazud/Desktop/Maestria/INVESTIGACION/Pruebas UNAM")
@@ -52,53 +67,19 @@ base <- read_excel(url,sheet = extremidad, col_names = FALSE)
 #Procesamiento
 ciclos<-cycle_process(base)
 cycles_plot(ciclos)
-ciclos2<-erase_cycles(ciclos,3,5,4)
-cycles_plot(ciclos2)
+ciclos_f<-erase_cycles(ciclos,13,5,4)
+cycles_plot(ciclos_f)
 
-C<-cbind("% ciclo"=c(0:100),data.frame(ciclos2))#En data frame
+C<-cbind("% ciclo"=c(0:100),data.frame(ciclos_f))#En data frame
 View(C)
 
-tema<-theme(axis.text=element_text(size=20),
-            axis.title=element_text(size=22,face="bold"),
-            plot.title = element_text(size = 30, face = "bold"))
+#####Ciclos finales en ggplot####
+all_ggplot(C)
 
-a1<-ggplot(data=C,aes(x=C[,1],y=C[,2],col=color[1]))+
-  geom_line(show.legend = FALSE)+
-  xlab("Ciclo [%]")+
-  ylab("Aceleración [g]")+
-  ggtitle("Ciclos Obtenidos")+tema
-  
-#####Ciclos a plotear####
-a2<-geom_line(data=C,aes(y=C[,3]),col=color[2])
-a3<-geom_line(data=C,aes(y=C[,4]),col=color[3])  
-a4<-geom_line(data=C,aes(y=C[,5]),col=color[4])  
-a5<-geom_line(data=C,aes(y=C[,6]),col=color[5])  
-a6<-geom_line(data=C,aes(y=C[,7]),col=color[6])  
-a7<-geom_line(data=C,aes(y=C[,8]),col=color[7])  
-a8<-geom_line(data=C,aes(y=C[,9]),col=color[8])  
-a9<-geom_line(data=C,aes(y=C[,10]),col=color[9])  
-a10<-geom_line(data=C,aes(y=C[,11]),col=color[10])  
-a11<-geom_line(data=C,aes(y=C[,12]),col=color[11])  
-a12<-geom_line(data=C,aes(y=C[,13]),col=color[12])  
-a13<-geom_line(data=C,aes(y=C[,14]),col=color[13])  
-a14<-geom_line(data=C,aes(y=C[,15]),col=color[14])  
-a15<-geom_line(data=C,aes(y=C[,16]),col=color[15])  
-a16<-geom_line(data=C,aes(y=C[,17]),col=color[16])  
-a17<-geom_line(data=C,aes(y=C[,18]),col=color[17])
-a18<-geom_line(data=C,aes(y=C[,19]),col=color[18])  
-a19<-geom_line(data=C,aes(y=C[,20]),col=color[19])  
-a20<-geom_line(data=C,aes(y=C[,21]),col=color[20])  
+prom<-as.data.frame(apply(C[,2:length(C)],1,mean))#aceleracion promedio
+desv<-as.data.frame(apply(C[,2:length(C)],1,sd))#desviacion estandar
 
-
-#####Preparación resultados####
-length(C)-1
-graf_ciclos<-a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12#+a13+a14+a15+a16+a17
-graf_ciclos
-
-prom<-as.data.frame(apply(C[,2:length(C)],1,mean))#Checar núm de ciclos+1
-desv<-as.data.frame(apply(C[,2:length(C)],1,sd))
-
-result<-cbind(C$porcentaje,prom,desv)
+result<-cbind(C$`% ciclo`,prom,desv)
 names(result)<-c("p_ciclo","promedio","desviación_estandar")
 plot.data<-cbind(result,lower=result$promedio-result$desviación_estandar,
                  upper=result$promedio+result$desviación_estandar)
@@ -108,7 +89,7 @@ graf<-ggplot(data=plot.data,aes(x=p_ciclo,y=promedio))+
   geom_line(col="red",size=1.2)+
   geom_ribbon(data=plot.data,aes(ymin=lower,ymax=upper,x=p_ciclo),
               alpha=0.3,colour="darkblue",fill="lightblue")
-
+graf
 graf_final<-graf+ggtitle("Aceleración Promedio y Desviación Estandar")+
   xlab("Porcentaje de Ciclo [%]")+
   ylab("Aceleración [g]")+tema
